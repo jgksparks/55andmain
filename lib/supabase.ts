51fd2f76d644aa66668016f8b24d856b8b28fdf4
Jeanne Kiernan <jeannekiernan@Jeannes-iMac.hsd1.ct.comcast.net>
2026-07-22 15:04:31 -0400

    Hardcode Supabase credentials to bypass Vercel env var issue

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const url = "https://nvaxpqwaqjwddofossdc.supabase.co";
+const anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52YXhwcXdhcWp3ZGRvZm9zc2RjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MjIwMTEsImV4cCI6MjA5ODQ5ODAxMX0.8y3SNtzde0IJxsjSR5lk3ZPN5q0vnaaUibS7G3Ibhco";
 
 export const supabase = createClient(url, anonKey);
 
